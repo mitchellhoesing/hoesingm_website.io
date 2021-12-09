dad43a7bb18d76ad7e8b78d2cfb180563923780f
Mitch <hoesingmitch02@gmail.com>
2021-12-08 16:44:11 -0800

    Fixed about section.

--- src/contents/About.js
+++ src/contents/About.js
@@ -14,9 +14,9 @@ class About extends Component {
                    that I am unable to write too much on for bias reasons. <br /><br />
 
                    I have a couple of goals currently. Number one is to get myself into an artificial intelligence role. My plan to achieve this involves going back to graduate
-                   school to provide myself with more time to study AI. After graduating with a Master's Degree, I will apply to positions that allow me to
-                   continue learning to eventually become an artificial intelligence consultant for businesses. I believe there is a large market for businesses that can operate
-                   more efficiently with the use of AI and I want to provide that service. Goal number two is to own real estate and other cash flowing assets. I will finance
+                   school to provide myself with more time to study AI. After graduation, I will apply to positions that allow me to
+                   continue learning and develop artificial intelligence that will aid humanity and reduce the suffering of those with basic human needs still unmet.
+                   Goal number two is to own real estate and other cash flowing assets. I will finance
                    these investments with my AI background and in time retire with assets to live from. <br /><br />
 
                    My hobbies include disc golfing, programming, running & weight lifting, reading, video gaming, garage sales, cooking, meditating, photography, art, biking and fishing.
